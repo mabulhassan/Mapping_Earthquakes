1d6e0a972e3b7406ab078648bde2c35a86fa5d0a
Maha Abul-Hassan <maha3220@yahoo.com>
2023-04-05 22:20:04 -0400

    add new files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,6 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([30, 30], 2);
 L.circle([34.0522, -118.2437], {
     radius: 100
  }).addTo(map);
@@ -12,6 +12,52 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: API_KEY
 });
-
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+  cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+  L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
